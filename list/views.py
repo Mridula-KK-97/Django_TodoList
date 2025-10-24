d513401ea786f3ad0f16c286c7d981060d70a00b
mridula <mridula8.97@gmail.com>
2025-10-24 13:50:41 +0530

    initial commit

--- list/views.py
+++ list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Task
+from django.contrib.auth.models import User
 
 def frontpage(request):
     tasks = Task.objects.all().order_by('-created_at')
@@ -53,3 +54,4 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('frontpage')
+
